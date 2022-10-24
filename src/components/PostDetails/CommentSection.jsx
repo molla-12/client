eb1f7847d56efe140fec9eae4dbfa5870fe91b95
molla-12 <mollaman19@gmail.com>
2022-10-24 11:37:30 +0300

    some change

--- src/components/PostDetails/CommentSection.jsx
+++ src/components/PostDetails/CommentSection.jsx
@@ -14,6 +14,8 @@ const CommentSection = ({ post }) => {
   const commentsRef = useRef();
 
   const handleComment = async () => {
+
+    if (!user) return "create account to comment";
     const newComments = await dispatch(commentPost(`${user?.result?.name}: ${comment}`, post._id));
 
     setComment('');
@@ -24,22 +26,22 @@ const CommentSection = ({ post }) => {
 
   return (
     <div>
-      <div className={classes.commentsOuterContainer}>
-        <div className={classes.commentsInnerContainer}>
+      <div className={ classes.commentsOuterContainer }>
+        <div className={ classes.commentsInnerContainer }>
           <Typography gutterBottom variant="h6">Comments</Typography>
-          {comments?.map((c, i) => (
-            <Typography key={i} gutterBottom variant="subtitle1">
-              <strong>{c.split(': ')[0]}</strong>
-              {c.split(':')[1]}
+          { comments?.map((c, i) => (
+            <Typography key={ i } gutterBottom variant="subtitle1">
+              <strong>{ c.split(': ')[0] }</strong>
+              { c.split(':')[1] }
             </Typography>
-          ))}
-          <div ref={commentsRef} />
+          )) }
+          <div ref={ commentsRef } />
         </div>
-        <div style={{ width: '70%' }}>
+        <div style={ { width: '70%' } }>
           <Typography gutterBottom variant="h6">Write a comment</Typography>
-          <TextField fullWidth rows={4} variant="outlined" label="Comment" multiline value={comment} onChange={(e) => setComment(e.target.value)} />
+          <TextField fullWidth rows={ 4 } variant="outlined" label="Comment" multiline value={ comment } onChange={ (e) => setComment(e.target.value) } />
           <br />
-          <Button style={{ marginTop: '10px' }} fullWidth disabled={!comment.length} color="primary" variant="contained" onClick={handleComment}>
+          <Button style={ { marginTop: '10px' } } fullWidth disabled={ !comment.length } color="primary" variant="contained" onClick={ handleComment }>
             Comment
           </Button>
         </div>
